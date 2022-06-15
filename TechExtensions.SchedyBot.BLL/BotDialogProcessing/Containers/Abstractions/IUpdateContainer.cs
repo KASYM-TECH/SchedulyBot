ed7221a4aebabb8fb657b7364a90122385616994
Tim45363 <kasymovty@gmail.com>
2022-06-15 14:00:11 +0300

    forth commit

--- TechExtensions.SchedyBot.BLL/BotDialogProcessing/Containers/Abstractions/IUpdateContainer.cs
+++
@@ -1,9 +0,0 @@
-﻿using Telegram.Bot.Types;
-
-namespace TechExtensions.SchedyBot.BLL.BotDialogProcessing.Containers.Abstractions
-{
-    public interface IUpdateContainer
-    {
-        public Update Update { get; set; }
-    }
-}
